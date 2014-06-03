c3b19d2acc77e039b8fdbbb8713eb59fdc7aa5b5
Ari Olmos <ari.olmos@gmail.com>
2014-06-02 22:45:02 -0700

    Created Post resource and updated index and show views

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Bloccit::Application.routes.draw do
-  get "welcome/index"
-  get "welcome/about"
-
+  resources :posts
+  get 'about' => 'welcome#about'
   root to: 'welcome#index'
 end  
   # The priority is based upon order of creation: first created -> highest priority.

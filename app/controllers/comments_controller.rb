c14b349227037aff42438bb9b5691d421b1f955c
Ari Olmos <ari.olmos@gmail.com>
2014-06-29 22:34:09 -0700

    files for comments

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,38 @@
+class CommentsController < ApplicationController
+  # def edit
+  #   @topic = Topic.find(params[:topic_id])
+  #   @post = Post.find(params[:post_id])
+  #   @comment = Comment.find(params[:id])
+  #   authorize @comment
+  # end
+
+  def create
+    @topic = Topic.find(params[:topic_id])
+    @post = @topic.posts.find(params[:post_id])
+    @comments = @post.comments
+
+    @comment = current_user.comments.build( comment_params )
+    @comment.post = @post
+    @new_comment = Comment.new
+
+
+    authorize @comment
+
+    if @comment.save
+      flash[:notice] = "Comment was saved successfully."
+    else
+      flash[:error] = "Error creating comment.  Please try again."
+    end
+    redirect_to [@topic, @post]
+  end 
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(
+      :body,
+      :post_id
+    )
+  end  
+
+end
